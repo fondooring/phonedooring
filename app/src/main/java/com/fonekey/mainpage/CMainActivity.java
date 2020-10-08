d7e930b6db6d378e3df2b932e7ea631948ffcbb8
Daniil Rybakin <greenrbk@gmail.com>
2020-10-08 07:43:19 +0300

    Add nivigation menu

--- app/src/main/java/com/fonekey/mainpage/CMainActivity.java
+++ app/src/main/java/com/fonekey/mainpage/CMainActivity.java
@@ -1,31 +1,63 @@
 package com.fonekey.mainpage;
-import com.fonekey.R;
 
-import android.os.Bundle;
 import android.content.Intent;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.android.material.tabs.TabLayout;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+import com.fonekey.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class CMainActivity extends AppCompatActivity {
 
-    public CMainActivity() {};
+    private static  final int m_layuot = R.layout.activity_main;
+
+    private DrawerLayout drawerLayout;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(m_layuot);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.mainTabLayout);
+        /*TabLayout tabLayout = (TabLayout) findViewById(R.id.mainTabLayout);
         ViewPager viewPager = (ViewPager) findViewById(R.id.mainViewPager);
 
         viewPager.setAdapter(new CPagerAdapter(getSupportFragmentManager()));
-        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);*/
+
+        DrawerLayout drawer = findViewById(R.id.drawerLayout);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.menu_search, R.id.menu_rent, R.id.menu_surrender).setDrawerLayout(drawer).build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 }
